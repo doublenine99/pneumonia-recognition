1ddfc5d6b5477e4ce6c8cead5c1c5b8faa7c9791
jiujiu <pjj851135136@gmail.com>
2020-04-11 22:47:43 -0500

    enhance contrast

--- Pneumonia_recgonition.m
+++ Pneumonia_recgonition.m
@@ -3,13 +3,20 @@ clear; clc; close all;
 
 %% determine condition to be severe by white area over threshould
 figure(1)
-I = imread('middle.png');
+% I = imread('good.png');
+I = rgb2gray(imread('middle.png'));
 subplot(1, 4, 1); imshow(I); title('Original Image');
+
+I = imadjust(I, [0.4 1]);
+subplot(1, 4, 2); imshow(I); title('after enhance contrast');
+
 [L, Centers] = imsegkmeans(I, 3);
 B = labeloverlay(I, L);
-subplot(1, 4, 2); imshow(B); title('Using K means to segment')
+subplot(1, 4, 3); imshow(B); title('Using K means to segment')
+
 gray = rgb2gray(B);
-subplot(1, 4, 3); imshow(gray);
+subplot(1, 4, 4); imshow(gray);
+
 % after_threshold = gray < 60;
 % subplot(1, 4, 4); imshow(after_threshold); title('after filter');
 % %Split into RGB Channels
