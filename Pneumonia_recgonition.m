5e8eda7d683ad6b094c2fff6e7e6e7892cd71e57
jiujiu <pjj851135136@gmail.com>
2020-04-21 18:31:24 -0500

    fill the edge

--- Pneumonia_recgonition.m
+++ Pneumonia_recgonition.m
@@ -6,19 +6,43 @@ figure(1)
 % I = imread('good.png');
 I = rgb2gray(imread('middle.png'));
 subplot(1, 4, 1); imshow(I); title('Original Image');
+% remove the coutour part
+mask = zeros(size(I));
+mask(25:end - 5, 25:end - 10) = 1;
+% imshow(mask); axis on;
+bw = activecontour(I, mask);
+% imshow(bw)
+for i = 1:size(I, 1)
 
-I = imadjust(I, [0.4 1]);
-subplot(1, 4, 2); imshow(I); title('after enhance contrast');
+    for j = 1:size(I, 2)
 
-[L, Centers] = imsegkmeans(I, 3);
+        if bw(i, j) == 0
+            
+            I(i, j) = 200;
+        end
+
+    end
+
+end
+
+subplot(1, 4, 2); imshow(I); title('remove contour');
+% I = imadjust(I, [0.2 1]);
+subplot(1, 4, 3); imshow(I); title('enhance contrast'); axis on;
+
+[L, Centers] = imsegkmeans(I, 2);
 B = labeloverlay(I, L);
-subplot(1, 4, 3); imshow(B); title('Using K means to segment')
-
+% subplot(1, 4, 4); imshow(B); title('Using K means to segment')
 gray = rgb2gray(B);
-subplot(1, 4, 4); imshow(gray);
+subplot(1, 4, 4); imshow(gray); title('K means to segment')
 
-% after_threshold = gray < 60;
+figure
+
+% subplot(1, 4, 1); imhist(I); title('');
+% BWc1 = imclearborder(B);
+% subplot(1, 4, 2); imshow(BWc1);
+% after_threshold = I < 60;
 % subplot(1, 4, 4); imshow(after_threshold); title('after filter');
+
 % %Split into RGB Channels
 % Red = B(:, :, 1);
 % Green = B(:, :, 2);
